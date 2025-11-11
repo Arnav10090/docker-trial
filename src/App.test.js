4196456124100c1c615242ce38a2a7c99307e007
Arnav Tiwari <arnavt292@gmail.com>
2025-11-11 15:17:33 +0530

    updated app.test.js file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders docker setup heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/sample project to test Docker setup/i);
+  expect(headingElement).toBeInTheDocument();
 });
